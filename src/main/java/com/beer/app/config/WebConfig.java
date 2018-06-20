beecd994ba853cc32f4e2e66ebcc22c8e7b2a30b
Anubhav Jain <32910680+Anubhavj02@users.noreply.github.com>
2018-06-21 00:30:27 +0100

    Update WebConfig.java

--- src/main/java/com/beer/app/config/WebConfig.java
+++ src/main/java/com/beer/app/config/WebConfig.java
@@ -43,7 +43,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
     }
 
 }
